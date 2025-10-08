aabec0d698add0c5b1931d71219ece2d9c7f87a3
Jershz <joshuavasher@comcast.net>
2025-10-08 14:01:37 -0400

    Setup library script and display placeholders on website

---
+++ library.js
@@ -0,0 +1,87 @@
+const websiteMain = document.querySelector(".main");
+const formDialog = document.querySelector(".dialog-form");
+const addNewBookButton = document.querySelector(".add-book-button");
+const submitNewBookButton = document.querySelector("#submit-button");
+const cancelFormButton = document.querySelector("#cancel-button");
+const myLibrary = [];
+const myLibraryDisplay = [];
+
+addNewBookButton.addEventListener("click", () => {
+    formDialog.querySelector("form").reset();
+    formDialog.showModal();
+});
+
+submitNewBookButton.addEventListener("click", () => {
+    const newBookTitle = formDialog.querySelector("#book-title");
+    const newBookAuthor = formDialog.querySelector("#author");
+    const newBookPages = formDialog.querySelector("#pages");
+    const newBookIsRead = formDialog.querySelector("#is-read");
+    addBookToLibrary(newBookAuthor.value,newBookTitle.value,newBookPages.value,newBookIsRead.checked);
+    formDialog.close();
+});
+
+cancelFormButton.addEventListener("click", () => {
+    formDialog.close();
+});
+
+function Book(author,title,pages,isRead) {
+    if(!new.target) {
+        throw("Must use 'new' keyword with Book constructor.");
+    }
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.isRead = isRead;
+    this.uniqueId = crypto.randomUUID();
+}
+
+function addBookToLibrary(author,title,pages,isRead) {
+    const newBook = new Book(author,title,pages,isRead);
+    myLibrary.push(newBook);
+    updateCardsOnWebsite();
+}
+
+addBookToLibrary("asd", "asddasd", 1000, true);
+addBookToLibrary("assdfsdd", "asd2345dasd", 10020, false);
+addBookToLibrary("awer234sd", "asdw34werdasd", 10400, true);
+
+function updateCardsOnWebsite() {
+    myLibrary.forEach((item,index) => {
+        if(myLibraryDisplay.includes(item)) {
+            return;
+        }
+        const newCard = document.createElement("div");
+        newCard.classList.add("card");
+
+        for(let prop in item) {
+            const newDiv = document.createElement("div");
+            if(prop === "isRead") {
+                const newCheck = document.createElement("input");
+                newCheck.type = "checkbox";
+                newCheck.checked = item[prop];
+                newDiv.textContent = "Read? ";
+                newDiv.appendChild(newCheck);
+            } else {
+                newDiv.classList.add(prop);
+                newDiv.textContent = item[prop];
+            }
+
+            if(prop === "uniqueId") {
+                newDiv.style.display = "none";
+            }
+            newCard.appendChild(newDiv);
+        }
+        const newRemoveButton = document.createElement("button");
+        newRemoveButton.textContent = "Remove";
+        newRemoveButton.addEventListener("click", () => {
+            let delId = newRemoveButton.parentElement.querySelector(".uniqueId").textContent;
+            const delIndex = myLibrary.findIndex(item => item.uniqueId === delId);
+            myLibrary.splice(delIndex, 1);            
+            newRemoveButton.parentElement.remove();
+        });
+        
+        newCard.appendChild(newRemoveButton);
+        websiteMain.appendChild(newCard);
+        myLibraryDisplay.push(item);
+    });
+}
